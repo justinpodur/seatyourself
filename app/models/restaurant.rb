f587540b307dba61f8ca51a4d2f77aa49811b5ae
git-justinpodur <git-justin@podur.org>
2014-06-26 14:28:41 -0400

    added reservations model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,6 @@
 class Restaurant < ActiveRecord::Base
 	validates :name, presence: true, length: {minimum: 5}
-	validates :pricerange, presence: true
-	validates :address, presence: true
-	validates :cuisine, presence: true
-	validates :summary, presence: true
-	validates :capacity, presence: true
-	validates :menu, presence: true
+	validates :pricerange, :address, :cuisine, :summary, :capacity, :menu, presence: true
+
+	has_many :reservations
 end
---
+++ db/migrate/20140626182356_create_reservations.rb
@@ -0,0 +1,12 @@
+class CreateReservations < ActiveRecord::Migration
+  def change
+    create_table :reservations do |t|
+      t.integer :party_size
+      t.datetime :party_time
+      t.integer :restaurant_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
