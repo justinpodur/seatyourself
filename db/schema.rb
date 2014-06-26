749ac4d5c4db355f45c3d7aaa2297dd7a8fe20bf
git-justinpodur <git-justin@podur.org>
2014-06-26 14:01:32 -0400

    created usermodel, controller and view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140625203818) do
+ActiveRecord::Schema.define(version: 20140626175248) do
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20140625203818) do
     t.text     "summary"
     t.string   "capacity"
     t.text     "menu"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
