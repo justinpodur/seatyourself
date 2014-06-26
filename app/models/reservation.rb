b0fac1db831723d233b168f3b0250c41f2ee92f7
git-justinpodur <git-justin@podur.org>
2014-06-26 17:21:45 -0400

    working on validation (with errors)

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,16 @@
 class Reservation < ActiveRecord::Base
 	belongs_to :restaurant
 	belongs_to :user
+
+# validates :availability
+
+	# private
+	
+	# def availability
+ #    if !restaurant.available?(party_size, party_time)
+ #        errors.add(:base, "Restaurant is full")
+ #    end
+	# end
+
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,20 @@ class Restaurant < ActiveRecord::Base
 	validates :pricerange, :address, :cuisine, :summary, :capacity, :menu, presence: true
 
 	has_many :reservations
+
+	def available(party_size,party_time)
+		booking=0
+		reservations = Reservation.where(@restaurant = @restaurant_id)
+		reservations.each do |r|
+			booking += r.party_size
+		end
+
+		if booking + party_size > @restaurant.capacity.to_i
+			return nil
+		else
+			return true
+		end
+
+	end
+
 end
